a0f2a61ab7643cacbc85f9456f33570ca326e59a
Dmitry <93837724+ramarioR@users.noreply.github.com>
2023-03-07 18:21:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -63,12 +63,12 @@ const renderAnimation = () => {
             // console.log(item.rotate);
             item.iteration = 100;
             item.posY += item.iteration / 216;
-            // item.el.style.top = `${item.posY}%`
-            item.el.style.transform = `translate3d(${item.posX}%, ${item.posY}%, 0) rotateZ(${item.rotate >= 1? item.rotateDeg = item.rotateDeg + 0.5 : item.rotateDeg = item.rotateDeg - 0.5}deg)`
+            item.el.style.top = `${item.posY}%`
+            item.el.style.transform = `rotateZ(${item.rotate >= 1? item.rotateDeg = item.rotateDeg + 0.5 : item.rotateDeg = item.rotateDeg - 0.5}deg)`
         })
 
         console.log(isMobile);
-        if (timer / interval > (isMobile ? 15 : 5)) {
+        if (timer / interval > (isMobile ? 20 : 5)) {
             timer = 0
 
             items.push(createItem())
